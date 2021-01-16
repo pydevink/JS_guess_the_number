e6b095515ae6b500f897ed7ac973d3b75a4ebdca
pydevink <djordje.ristic@sbb.rs>
2021-01-16 12:35:25 +0100

    removed console logs

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ const displayMessage = function (message) {
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
 
     // When there is no input
     if (!guess) {
